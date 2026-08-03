bd2625182ef9190db5bf2d25471e5887f9493e1e
Gabriel dos Santos Vereda <gabrielsantosvereda@gmail.com>
2026-08-03 10:23:55 -0300

    feat: adicionar rotas para navegação e configurar layout principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: 'bookings',
+    loadComponent: () => import('./features/bookings/bookings').then(m => m.Bookings)
+  },
+  {
+    path: 'payment',
+    loadComponent: () => import('./features/payment/payment').then(m => m.Payment)
+  },
+  {
+    path: 'chat',
+    loadComponent: () => import('./features/chat/chat').then(m => m.Chat)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/profile').then(m => m.Profile)
+  }
+];
