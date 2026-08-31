5cf8c99444310ae889fe72c70c5022ed07fb2f3a
GABRIEL DE LIMA SILVA <gabriel.silva1451@senaisp.edu.br>
2026-08-31 11:03:56 -0300

    atualização

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Button } from './components/atoms/button/button';
-import { IconButton } from "./components/atoms/icon-button/icon-button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Button, IconButton, Button],
+  imports: [RouterOutlet, Button],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/atoms/button/button.ts
+++ src/app/components/atoms/button/button.ts
@@ -1,12 +1,27 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
+export type buttonVariant = 'primary' | 'secondary' | 'google' | 'notification' | 'filter';
+
+const variantIcon: Record<buttonVariant, string | null> = {
+  primary: null,
+  secondary: null,
+  google: 'google-icon.svg',
+  notification: 'notification-icon.svg',
+  filter: 'filter-icon.svg',
+};
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   templateUrl: './button.html',
-  styleUrl: './button.scss'
+  styleUrl: './button.scss',
 })
 export class Button {
-  variant = input<'primary' | 'secondary' | 'google'>('primary');
+  variant = input<buttonVariant>('primary');
+  text = input<string>('');
+
+  iconPath = computed(() => {
+    const fileName = variantIcon[this.variant()];
+    return fileName ? `assets/icons/${fileName}` : null;
+  });
 }
