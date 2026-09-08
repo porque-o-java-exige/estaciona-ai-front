1df23bab0959629537d9d77bace7ad5e3de0fdcc
Gabriel dos Santos Vereda <gabrielsantosvereda@gmail.com>
2026-09-07 23:16:34 -0300

    feat: reorganizar estrutura do app e atualizar testes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home';
+import { ComponentDetailPage } from './pages/component-detail/component-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'component/:id', component: ComponentDetailPage },
+  { path: '**', redirectTo: '' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Chip } from './components/atoms/chip/chip';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    Chip
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('estacionaAi');
